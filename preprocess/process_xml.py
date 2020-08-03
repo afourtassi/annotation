9c642ac72e9ba245119aabc36d482db14081c6d7
madcpt <zihan.xu@sjtu.edu.cn>
2020-08-03 22:52:57 +0800

    add preprocess pipeline

--- preprocess/process_xml.py
+++ preprocess/process_xml.py
@@ -1,24 +1,26 @@
 import os
+from os import system
 import sys
 import json
 from parser import translate_to_json
+from threading import main_thread
 from xml.etree.ElementTree import parse, fromstring, ElementTree
 
 
-input_dir = 'corpus/Paris-xml'
-data_root = 'data'
-output_dir = 'Paris'
+if __name__ == "__main__":
+    assert len(sys.argv) == 4 and sys.argv[0].endswith('.py')
+    input_dir = sys.argv[1]     # 'corpus/Paris-xml'
+    data_root = sys.argv[2]     # 'data'
+    output_dir = sys.argv[3]    # 'Paris'
 
-for sub_path in os.listdir(input_dir):
-    if not os.path.exists(os.path.join(data_root, output_dir, sub_path)):
-        os.makedirs(os.path.join(data_root, output_dir, sub_path))
-    for filename in os.listdir(os.path.join(input_dir, sub_path)):
-        doc = parse(os.path.join(input_dir, sub_path, filename))
-        # print(sub_path)
-        # print(filename)
-        filename = os.path.join(output_dir, sub_path, filename.replace('.xml', '.json'))
-        d = translate_to_json(doc, filename)
-        with open(os.path.join(data_root, filename), 'w') as f:
-            json.dump(d, f, indent=4)
-    #     break
-    # break
+    for sub_path in os.listdir(input_dir):
+        if not os.path.exists(os.path.join(data_root, output_dir, sub_path)):
+            os.makedirs(os.path.join(data_root, output_dir, sub_path))
+        for filename in os.listdir(os.path.join(input_dir, sub_path)):
+            doc = parse(os.path.join(input_dir, sub_path, filename))
+            # print(sub_path)
+            # print(filename)
+            filename = os.path.join(output_dir, sub_path, filename.replace('.xml', '.json'))
+            d = translate_to_json(doc, filename)
+            with open(os.path.join(data_root, filename), 'w') as f:
+                json.dump(d, f, indent=4)
---
+++ preprocess/run_me.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+CORPUS_TMP="corpus_tmp/"
+DATA_ROOT="data/"
+
+if [ -d $CORPUS_TMP ]
+then
+    echo "$CORPUS_TMP already existed."
+else
+    echo "$CORPUS_TMP not found, make new dir."
+    mkdir "$CORPUS_TMP"
+fi
+
+if [ -d $DATA_ROOT ]
+then
+    echo "$DATA_ROOT already existed."
+else
+    echo "$DATA_ROOT not found, make new dir."
+    mkdir "$DATA_ROOT"
+fi
+
+if [ -e "$CORPUS_TMP/chatter.jar" ]
+then
+    echo "CHATTER already prepared, no need to downlaod."
+else
+    echo "CHATTER not found, start downloading."
+    wget -P $CORPUS_TMP https://talkbank.org/software/chatter.jar
+fi
+
+if [ "$#" -eq 1 ]
+then
+    if [ ! -d "$DATA_ROOT/$1" ]
+    then
+        echo "Downloading: https://phonbank.talkbank.org/data/French/$1.zip"
+        wget -P "$CORPUS_TMP/" "https://phonbank.talkbank.org/data/French/$1.zip"
+        unzip "$CORPUS_TMP/$1.zip" -d "$CORPUS_TMP"
+        java -cp chatter.jar org.talkbank.chatter.App --inputFormat cha --outputFormat xml --outputDir "$CORPUS_TMP/$1-xml" -tree "$CORPUS_TMP/$1"
+        python3 preprocess/process_xml.py "$CORPUS_TMP/$1-xml" "$DATA_ROOT" "$1"
+    else
+        echo "Corpus $1 has been processed."
+    fi
+    ./tools/update_list_file.sh "$DATA_ROOT"
+    echo ""
+    echo "Preprocessing for $1 completed!"
+else
+    echo "Please provide Corpus! For example, 'Paris'."
+    exit
+fi
